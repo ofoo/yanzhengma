0929d0135466ab5e2634bdaab96a573dd2944750
郭超 <guochao@268xue.com>
2019-09-30 17:27:30 +0800

    提交代码

---
+++ src/main/java/com/guoguo/yanzhengma/YanzhengmaApplication.java
@@ -0,0 +1,13 @@
+package com.guoguo.yanzhengma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanzhengmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanzhengmaApplication.class, args);
+    }
+
+}
